c73d4a321f191c83c76944cdd57fa7679c99b829
Hibahalatawi <hibahapple22@gmail.com>
2023-02-08 09:43:57 +0300

    Initial Commit

---
+++ scanner/scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scannerApp.swift
+//  scanner
+//
+//  Created by Hibah Abdullah Alatawi on 17/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
